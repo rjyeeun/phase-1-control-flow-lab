10145c5a67528d252b838f84f02943fd40ab6364
Yeeun Jung <rjyeeun@gmail.com>
2022-12-12 13:38:56 -0500

    commit tree 94b22d33cc2c1e13bec319b88c496b852f297630

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(aNumber){
   // Write your code here!
+  if (aNumber <= 400) {
+    return 'This one is on me!'
+  }
+  else if (aNumber <2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if (aNumber > 2000 && aNumber < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (aNumber > 2500) {
+    return 'No can do.'
+  }
+  
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  }
+  else {return 'No go.'}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  if (tip === "generous") {
+    return "Thank you so much."
+    // Write your code here!
 }
+ else if (tip === "not as generous") {
+  return 'Thank you.'
+ }
+ else {return 'Bye.'}
+ }
